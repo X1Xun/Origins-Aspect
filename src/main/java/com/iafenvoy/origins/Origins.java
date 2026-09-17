b91b6a730847be5813c128abbc8088be45b2a7d4
XiXun <theframe666@gmail.com>
2026-09-17 18:27:52 +0300

    пару способок

--- src/main/java/com/iafenvoy/origins/Origins.java
+++ src/main/java/com/iafenvoy/origins/Origins.java
@@ -2,6 +2,7 @@ package com.iafenvoy.origins;
 
 import com.iafenvoy.jupiter.ConfigManager;
 import com.iafenvoy.jupiter.ServerConfigManager;
+import com.iafenvoy.origins.attachment.PowerHelper;
 import com.iafenvoy.origins.config.OriginsConfig;
 import com.iafenvoy.origins.content.IconItems;
 import com.iafenvoy.origins.content.ModEffects;
@@ -18,14 +19,27 @@ import com.iafenvoy.origins.data.power.builtin.PreventPowers;
 import com.iafenvoy.origins.data.power.builtin.RegularPowers;
 import com.iafenvoy.origins.data.power.builtin.modify.ModifyPotionDurationPower;
 import com.iafenvoy.origins.data.power.builtin.prevent.PreventBlockTriggerPower;
+import com.iafenvoy.origins.data.power.builtin.regular.HolyBoltPower;
+import com.iafenvoy.origins.data.power.builtin.regular.WarlockMainPower;
 import com.iafenvoy.origins.data.power.component.BuiltinComponents;
 import com.iafenvoy.origins.registry.*;
 import com.mojang.logging.LogUtils;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.npc.Villager;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.trading.MerchantOffer;
+import net.minecraft.world.item.trading.MerchantOffers;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
 import net.neoforged.neoforge.event.entity.living.LivingFallEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.neoforge.event.tick.PlayerTickEvent;
 import org.slf4j.Logger;
 
 @Mod(Origins.MOD_ID)
@@ -40,9 +54,69 @@ public final class Origins {
         }
     }
 
+    @SubscribeEvent
+    public static void onVillagerInteract(PlayerInteractEvent.EntityInteract event) {
+        if (event.getTarget() instanceof Villager villager) {
+            Player player = event.getEntity();
+            var list = PowerHelper.get(player).listActive(WarlockMainPower.class);
+            if (!list.isEmpty()) {
+                if (!player.level().isClientSide()) {
+                    MerchantOffers offers = villager.getOffers();
+                    for (MerchantOffer offer : offers) {
+                        int baseCost = offer.getBaseCostA().getCount();
+                        int priceIncrease = (int) (baseCost);
+                        offer.setSpecialPriceDiff(priceIncrease);
+                    }
+                }
+            } else {
+
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onVillagerDeath(LivingDeathEvent event) {
+        try {
+            Player player = (Player) event.getEntity().getLastAttacker();
+            var list = PowerHelper.get(player).listActive(HolyBoltPower.class);
+            if (!list.isEmpty()) {
+                if (event.getEntity() instanceof Villager villager) {
+                    if (event.getSource().getEntity() instanceof Player player1) {
+                        player.hurt(player.damageSources().playerAttack(player1), 3.0F);
+                    }
+                }
+            } else {
+
+            }
+        }
+        catch (Exception ex){
+
+        }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerTick(PlayerTickEvent.Post event) {
+        Player player = event.getEntity();
+        var list = PowerHelper.get(player).listActive(HolyBoltPower.class);
+        if (!player.level().isClientSide() && player.isAlive() && !list.isEmpty()) {
+            if (player.level().isRainingAt(player.blockPosition())) {
+                player.addEffect(new MobEffectInstance(
+                        MobEffects.MOVEMENT_SLOWDOWN,
+                        200,
+                        0,
+                        false,
+                        true
+                ));
+            }
+        }
+    }
+
     public Origins(IEventBus bus) {
         NeoForge.EVENT_BUS.addListener(Origins::onLivingFall);
+        NeoForge.EVENT_BUS.addListener(Origins::onVillagerInteract);
         NeoForge.EVENT_BUS.addListener(ModifyPotionDurationPower::onItemUseFinish);
+        NeoForge.EVENT_BUS.addListener(Origins::onVillagerDeath);
+        NeoForge.EVENT_BUS.addListener(Origins::onPlayerTick);
         ModEffects.register(bus);
         ConfigManager.getInstance().registerServerConfigHandler(OriginsConfig.INSTANCE, ServerConfigManager.PermissionChecker.IS_OPERATOR);
         ModItems.register(bus);

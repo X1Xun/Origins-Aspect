6ee75b9a93f77b60179bc4a9b7d604c72fe0c49f
XiXun <theframe666@gmail.com>
2026-09-17 18:34:11 +0300

    пару способок

--- src/main/java/com/iafenvoy/origins/content/ModItems.java
+++ src/main/java/com/iafenvoy/origins/content/ModItems.java
@@ -10,25 +10,25 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Origins.MOD_ID);
 
-    public static final DeferredHolder<Item, WarlockScrollItem> SCROLL_EARTH = ITEMS.register("warlock_scroll_earth",
+    public static final DeferredHolder<Item, WarlockScrollItem> SCROLL_EARTH = ITEMS.register("scroll_of_earth",
             () -> new WarlockScrollItem(new Item.Properties().stacksTo(1), warlock_powers.EARTH, "Земли"));
 
-    public static final DeferredHolder<Item, WarlockScrollItem> SCROLL_FROST = ITEMS.register("warlock_scroll_frost",
+    public static final DeferredHolder<Item, WarlockScrollItem> SCROLL_FROST = ITEMS.register("scroll_of_frozen",
             () -> new WarlockScrollItem(new Item.Properties().stacksTo(1), warlock_powers.FROST, "Вечного Мороза"));
 
-    public static final DeferredHolder<Item, WarlockScrollItem> SCROLL_INFERNAL = ITEMS.register("warlock_scroll_infernal",
+    public static final DeferredHolder<Item, WarlockScrollItem> SCROLL_INFERNAL = ITEMS.register("scroll_of_infernal",
             () -> new WarlockScrollItem(new Item.Properties().stacksTo(1), warlock_powers.INFERNAL, "Ада"));
 
     public static final DeferredHolder<Item, WarlockScrollItem> SCROLL_DRAGON = ITEMS.register("warlock_scroll_dragon",
             () -> new WarlockScrollItem(new Item.Properties().stacksTo(1), warlock_powers.DRAGON, "Дракона"));
 
-    public static final DeferredHolder<Item, WarlockScrollItem> SCROLL_ANCIENT = ITEMS.register("warlock_scroll_ancient",
+    public static final DeferredHolder<Item, WarlockScrollItem> SCROLL_ANCIENT = ITEMS.register("scroll_of_ancient",
             () -> new WarlockScrollItem(new Item.Properties().stacksTo(1), warlock_powers.ANCIENT, "Древних"));
 
-    public static final DeferredHolder<Item, WarlockScrollItem> SCROLL_PILLAGER = ITEMS.register("warlock_scroll_pillager",
+    public static final DeferredHolder<Item, WarlockScrollItem> SCROLL_PILLAGER = ITEMS.register("scroll_of_pillager",
             () -> new WarlockScrollItem(new Item.Properties().stacksTo(1), warlock_powers.PILLAGER, "Разбойников"));
 
-    public static final DeferredHolder<Item, WarlockScrollItem> SCROLL_WIND = ITEMS.register("warlock_scroll_wind",
+    public static final DeferredHolder<Item, WarlockScrollItem> SCROLL_WIND = ITEMS.register("scroll_of_wind",
             () -> new WarlockScrollItem(new Item.Properties().stacksTo(1), warlock_powers.WIND, "Ветра"));
 
     public static final java.util.function.Supplier<Item> GRAPPLING_HOOK = ITEMS.registerItem(
